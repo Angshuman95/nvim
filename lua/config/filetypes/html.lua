117ce0f7433e15dfeb3bfbec1f782fde3a5ff8ff
Angshuman <ghosh.angshuman1@gmail.com>
2021-06-27 01:21:17 +0530

    Added Filetype tabstop option

---
+++ lua/config/filetypes/html.lua
@@ -0,0 +1,8 @@
+local O = require('utils.option')
+
+O.g({
+    tabstop = 2,
+    softtabstop = 2,
+    shiftwidth = 2,
+    expandtab = true,
+})
---
+++ lua/config/filetypes/init.lua
@@ -0,0 +1,4 @@
+require('config.filetypes.html')
+require('config.filetypes.css')
+require('config.filetypes.markdown')
+require('config.filetypes.json')
