35afbd7b5d894a2be51842018e2911247cd8d326
Angshuman <ghosh.angshuman1@gmail.com>
2021-07-05 22:16:54 +0530

    Separated autogroup and command utility function

---
+++ lua/utils/command.lua
@@ -0,0 +1,15 @@
+local C = {}
+
+C.commands = function (commands)
+    for name, c in pairs(commands) do
+        local command
+        if c.buffer then
+            command = 'command!-buffer'
+        else
+            command = 'command!'
+        end
+        vim.cmd(command .. ' -nargs=' .. c.nargs .. ' ' .. name .. ' ' .. c.cmd)
+    end
+end
+
+return C
