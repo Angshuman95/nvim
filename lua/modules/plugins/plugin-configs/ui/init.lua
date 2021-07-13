5f25203994f6f7aa260ab5ead153c71188f2ceec
Angshuman <ghosh.angshuman1@gmail.com>
2021-07-14 02:16:45 +0530

    NvimTree config additions

--- lua/modules/plugins/plugin-configs/ui/init.lua
+++ lua/modules/plugins/plugin-configs/ui/init.lua
@@ -219,8 +219,10 @@ function config.tree()
     vim.g.nvim_tree_indent_markers = 1
     vim.g.nvim_tree_follow = 1
     vim.g.nvim_tree_lsp_diagnostics = 1
-    vim.g.nvim_tree_auto_close = true
+    vim.g.nvim_tree_auto_close = 1
     vim.g.nvim_tree_auto_ignore_ft = {'startify', 'dashboard'}
+    vim.g.nvim_tree_git_hl = 1
+    vim.g.nvim_tree_highlight_opened_files = 1
     vim.g.nvim_tree_show_icons = {
         git = 1,
         folders = 1,
