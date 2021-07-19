65f7091b2e5f71872e5dd512134a42c7ccf48e18
Angshuman <ghosh.angshuman1@gmail.com>
2021-07-20 01:44:38 +0530

    Restore cursor of terminal to blinking I-Block

--- lua/config/autogroups.lua
+++ lua/config/autogroups.lua
@@ -3,5 +3,13 @@ local A = require('utils.autogroup')
 A.augroups({
     highlight_yank = {
         'TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=300}'
+    },
+    set_cursor_shape = {
+        'VimEnter,VimResume * set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50' ..
+            ',a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor' ..
+            ',sm:block-blinkwait175-blinkoff150-blinkon175'
+    },
+    restore_cursor_shape = {
+        'VimLeave,VimSuspend * set guicursor=a:ver90-blinkwait300-blinkon200-blinkoff150'
     }
 })
