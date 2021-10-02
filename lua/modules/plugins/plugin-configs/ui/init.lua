1e3846910e56207dc8ebb2c870b62949d7f9edd0
Angshuman <ghosh.angshuman1@gmail.com>
2021-10-02 14:30:53 +0530

    Kept the tree indent markers

--- lua/modules/plugins/plugin-configs/ui/init.lua
+++ lua/modules/plugins/plugin-configs/ui/init.lua
@@ -254,6 +254,7 @@ function config.tree()
     vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
     vim.g.nvim_tree_hide_dotfiles = 1
     vim.g.nvim_tree_git_hl = 1
+    vim.g.nvim_tree_indent_markers = 1
     vim.g.nvim_tree_highlight_opened_files = 1
     vim.g.nvim_tree_show_icons = {
         git = 1,
